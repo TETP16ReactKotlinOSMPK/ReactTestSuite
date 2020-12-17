10f39e89953faf5d129dba80cfb65d171f7336ee
osksvn <osksvn@outlook.com>
2020-12-17 23:17:31 +0100

    changed some styling

--- src/scenes/testScenario1/index.js
+++ src/scenes/testScenario1/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import {SafeAreaView, Text, TouchableHighlight, TouchableOpacity, View, StyleSheet} from 'react-native';
+import {
+  SafeAreaView,
+  Text,
+  TouchableHighlight,
+  TouchableOpacity,
+  View,
+  StyleSheet,
+} from 'react-native';
 import GetLocation from 'react-native-get-location';
-import { RNCamera } from 'react-native-camera';
+import {RNCamera} from 'react-native-camera';
 
 const TestScenario1Screen = ({navigation}) => (
-    <View style={styles.container}>
+  <View style={styles.container}>
     <RNCamera
-      ref={ref => {
+      ref={(ref) => {
         this.camera = ref;
       }}
       style={styles.preview}
@@ -24,59 +31,61 @@ const TestScenario1Screen = ({navigation}) => (
         buttonPositive: 'Ok',
         buttonNegative: 'Cancel',
       }}
-      onGoogleVisionBarcodesDetected={({ barcodes }) => {
+      onGoogleVisionBarcodesDetected={({barcodes}) => {
         console.log(barcodes);
       }}
     />
-    <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
-      <TouchableOpacity onPress={this.takePicture.bind(this)} style={styles.capture}>
-        <Text style={{ fontSize: 14 }}> SNAP </Text>
+    <View style={{flex: 0, flexDirection: 'row', justifyContent: 'center'}}>
+      <TouchableOpacity
+        onPress={this.takePicture.bind(this)}
+        style={styles.capture}>
+        <Text style={{fontSize: 14}}> TAKE PHOTO </Text>
       </TouchableOpacity>
     </View>
   </View>
 );
 
 takePicture = async () => {
-    if (this.camera) {
-      // Get gps location
-      GetLocation.getCurrentPosition({
-        enableHighAccuracy: true,
-        timeout:  1500,
-      })
-      .then(location => {
+  if (this.camera) {
+    // Get gps location
+    GetLocation.getCurrentPosition({
+      enableHighAccuracy: true,
+      timeout: 1500,
+    })
+      .then((location) => {
         console.log(location);
       })
-      .catch(error => {
-        const { code, message} = error;
+      .catch((error) => {
+        const {code, message} = error;
         console.warn(code, message);
-      })
-      // Take picture
-      const options = { quality: 0.5, base64: true };
-      const data = await this.camera.takePictureAsync(options);
-      console.log(data.uri);
-    }
-  };
+      });
+    // Take picture
+    const options = {quality: 0.5, base64: true};
+    const data = await this.camera.takePictureAsync(options);
+    console.log(data.uri);
+  }
+};
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      flexDirection: 'column',
-      backgroundColor: 'black',
-    },
-    preview: {
-      flex: 1,
-      justifyContent: 'flex-end',
-      alignItems: 'center',
-    },
-    capture: {
-      flex: 0,
-      backgroundColor: '#fff',
-      borderRadius: 5,
-      padding: 15,
-      paddingHorizontal: 20,
-      alignSelf: 'center',
-      margin: 20,
-    },
-  });
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'black',
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: 'center',
+    margin: 20,
+  },
+});
 
 export default TestScenario1Screen;
