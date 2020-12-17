3a2c9364533de007145c1cbf6bc85f4c6502359f
osksvn <osksvn@outlook.com>
2020-12-17 23:16:30 +0100

    changed some styling

--- src/scenes/home/index.js
+++ src/scenes/home/index.js
@@ -1,14 +1,38 @@
 import React from 'react';
-import {SafeAreaView, Text, TouchableHighlight} from 'react-native';
+import {SafeAreaView, Text, TouchableHighlight, StyleSheet} from 'react-native';
 
 const HomeScreen = ({navigation}) => (
-    <SafeAreaView>
-        <Text>Screen : Home</Text>
-
-        <TouchableHighlight onPress={() => navigation.navigate('TestScenario1')}>
-            <Text>Go to test scenario 1</Text>
-        </TouchableHighlight>
-    </SafeAreaView>
+  <SafeAreaView style={styles.container}>
+    <TouchableHighlight
+      onPress={() => navigation.navigate('TestScenario1')}
+      style={styles.button}>
+      <Text style={{fontSize: 14}}>Go to test scenario 1</Text>
+    </TouchableHighlight>
+  </SafeAreaView>
 );
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'white',
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  button: {
+    flex: 0,
+    backgroundColor: 'white',
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: 'center',
+    margin: 20,
+    borderColor: 'black',
+    borderWidth: 1,
+  },
+});
 
 export default HomeScreen;
